5fe8a68b48046f72bcc6d9a323649be94973006d
aidilbaihaqi <aidillmusirjun@gmail.com>
2024-09-25 13:49:25 +0700

    learn hello world & data types (integer, string, float)

---
+++ basic.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+  // Hello World
+  printf("Hello, Aidil\n");
+
+  // Data Types
+  int x=1, y=2; /* Integer Types */
+  float xc = 0.555; /* Bilangan Rill */
+  float cx = 1.5E3;
+
+  x+=y;
+  printf("x + y = %d \n", x);  
+  printf("value xc : %5.2f \n", xc);
+  printf("value cx : %10.2f \n", cx);
+  return 0;
+}
