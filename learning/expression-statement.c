013a27903e524f8ec6e784f1ebb23e45956d8e9e
aidilbaihaqi <aidillmusirjun@gmail.com>
2024-11-06 13:42:56 +0700

    pertemuan 9 struktur data: expression statement

---
+++ learning/expression-statement.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+  
+  // Postfix Expression
+  //  A B +
+  //  A B * C +
+  // dapat diselesaikan dengan stack
+
+  // Cara konversi infix to postfix
+  // Apabila ditemukan operand (variable) maka dituliskan pada hasil
+  // Apabila ditemukan operator aritmetik atau tanda kurung (maka dimasukkan ke stack (push))
+  // Apabila ditemukan tanda kurung ) maka seluruh stack dikeluarkan dengan pop sampai menemukan kurung buka baru
+
+  // Infix = a + b * c + (d * e + f) * g (manusia mengerjakan dengan cara ini)
+  // Postfix = a b c * + d e * f + g * + (komputer mengerjakan dengan cara ini)
+
+  return 0;
+}
