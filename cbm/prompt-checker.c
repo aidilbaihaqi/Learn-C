3f29e7860572cdded5aa4702ba4563931837041f
aidilbaihaqi <aidillmusirjun@gmail.com>
2024-11-07 07:32:27 +0700

    cbm 2

---
+++ cbm/prompt-checker.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+  // Kita nerima input dari user
+
+  // mengecek bagaimana komputer dapat mengenali subject, predikat, dan objek dari inputan
+  // formula dasar = subject + predikat + object
+
+  // jika hasilnya acak, maka inputan tidak valid
+
+  return 0;
+}
